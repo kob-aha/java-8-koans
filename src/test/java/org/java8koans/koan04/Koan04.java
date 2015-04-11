59fdbd723e4d26c71ea444ad6f8bfb6b8043fc00
Koby Aharon <kobyahron@gmail.com>
2015-04-11 23:32:21 +0300

    Koan04 - WIP

---
+++ src/test/java/org/java8koans/koan04/Koan04.java
@@ -0,0 +1,14 @@
+package org.java8koans.koan04;
+
+/**
+ * Koan04 - Streams
+ *
+ * Resource list:
+ *
+ *   http://www.javacodegeeks.com/2014/05/java-8-features-tutorial.html#streams
+ *   https://docs.oracle.com/javase/tutorial/collections/streams/index.html
+ *   http://www.oracle.com/technetwork/articles/java/ma14-java-se-8-streams-2177646.html
+ *   https://docs.oracle.com/javase/8/docs/api/java/util/stream/package-summary.html
+ */
+public class Koan04 {
+}
